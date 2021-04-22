83b9343f93f2b9496def4f8d28c3d7eb744dc0f5
Giorgos Trogadas <trogadas@kth.se>
2021-04-22 20:50:52 +0200

    UPD: The convert_to_tif.py now actually does what it says. It will convert to tif using LAB color space.

--- src/convert_to_tif.py
+++ src/convert_to_tif.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageCms
 from tqdm import tqdm
 import os
 
@@ -7,29 +7,35 @@ import os
 """ 
 Extract the dataset (folders test, train, val) and place them into folder dataset.
 Then run this script to convert the .JPEG images to .TIF images.
+
+https://stackoverflow.com/questions/52767317/how-to-convert-rgb-image-pixels-to-lab/53353542#53353542
 """
 
 
-def turn_BW():
-    os.makedirs('./train_BW/', exist_ok = True)
-    os.makedirs('./validation_BW/', exist_ok = True)
-    os.makedirs('./test_BW/', exist_ok = True)
-    paths = ['./test/', './val/', './train/']
-    
-    for path in paths:
-        path = Path(path)
-        pic_list = list(path.glob('**/*.JPEG'))
+def convert_to_tif():
+	os.makedirs('../dataset/train_tif/', exist_ok = True)
+	os.makedirs('../dataset/validation_tif/', exist_ok = True)
+	os.makedirs('../dataset/test_tif/', exist_ok = True)
+	paths = ['../dataset/test/', '../dataset/val/', '../dataset/train/']
+	
+	for path in paths:
+		path = Path(path)
+		pic_list = list(path.glob('**/*.JPEG'))
 
-        for pic in tqdm(pic_list):
-            with Image.open(pic) as im:
-                gs = ImageOps.grayscale(im)
-                first_word = str(pic).split('/')[0]
-                save_path = str(pic).replace(first_word+'/' , first_word + '_BW/')
-                save_path_dir = ''.join([word+'/' for word in save_path.split('/')[:-1]])
-                os.makedirs(save_path_dir, exist_ok = True)
-                gs.save(save_path, 'JPEG')
+		srgb_p = ImageCms.createProfile("sRGB")
+		lab_p  = ImageCms.createProfile("LAB")
+		rgb_to_lab = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, "RGB", "LAB")
 
-turn_BW()
+		for pic in tqdm(pic_list):
+			with Image.open(pic).convert('RGB') as im:
+				tif_img = ImageCms.applyTransform(im, rgb_to_lab)
 
-# 'test/images/test_3736.JPEG'
-# 'test_BW/images/test_3736.JPEG'
+				word = str(pic).split('/')[2]
+				save_path = str(pic).replace(word+'/' , word + '_tif/')
+				save_path = save_path.replace('.JPEG', '.TIF')
+				save_path_dir = ''.join([w+'/' for w in save_path.split('/')[:-1]])
+				os.makedirs(save_path_dir, exist_ok = True)
+				tif_img.save(save_path, 'TIFF')
+
+if __name__ == '__main__':
+	convert_to_tif()
--- src/data_loader.py
+++ src/data_loader.py
@@ -5,29 +5,29 @@ import numpy as np
 
 
 class ConcatDataset(torch.utils.data.Dataset):
-    def __init__(self, paths, stringToReplace, replacementString):
+	def __init__(self, paths, stringToReplace, replacementString):
 
-        self.paths = paths
-        # targetPaths = [Path(str(path).replace(stringToReplace, replacementString)) for path in self.paths]
-        # self.targetPaths = targetPaths
-        # print(self.targetPaths[0])
-        # print("original path: ", self.paths[0])
+		self.paths = paths
+		# targetPaths = [Path(str(path).replace(stringToReplace, replacementString)) for path in self.paths]
+		# self.targetPaths = targetPaths
+		# print(self.targetPaths[0])
+		# print("original path: ", self.paths[0])
 
 
 
-        
-        
+		
+		
 
-    def __getitem__(self, i):
-        colorImg = Image.open(self.paths[i])
-        # bwImg = Image.open(self.targetPaths[i])
-        # colorImg = np.array(colorImg)
-        # bwImg = np.array(bwImg)
-        return colorImg
-        
+	def __getitem__(self, i):
+		colorImg = Image.open(self.paths[i])
+		# bwImg = Image.open(self.targetPaths[i])
+		# colorImg = np.array(colorImg)
+		# bwImg = np.array(bwImg)
+		return colorImg
+		
 
-    def __len__(self):
-        return len(self.paths)
+	def __len__(self):
+		return len(self.paths)
 
 
 
