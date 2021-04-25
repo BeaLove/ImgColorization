1832b7c0a07090abc6cfb18349ef019f4366e72e
Giorgos Trogadas <trogadas@kth.se>
2021-04-25 15:12:24 +0200

    Working data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,42 +3,28 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 
-
-class ConcatDataset(torch.utils.data.Dataset):
-	def __init__(self, paths, stringToReplace, replacementString):
-
-		self.paths = paths
-		# targetPaths = [Path(str(path).replace(stringToReplace, replacementString)) for path in self.paths]
-		# self.targetPaths = targetPaths
-		# print(self.targetPaths[0])
-		# print("original path: ", self.paths[0])
-
-
-
-		
-		
+class Dataset(torch.utils.data.Dataset):
+	def __init__(self, dataset):
+		self.dataset = dataset
 
 	def __getitem__(self, i):
-		colorImg = Image.open(self.paths[i])
-		# bwImg = Image.open(self.targetPaths[i])
-		# colorImg = np.array(colorImg)
-		# bwImg = np.array(bwImg)
-		return colorImg
+		im = Image.open(self.dataset[i])
+		im = np.array(im)
+		X, y = im[:,:,0], im[:,:,1:]
+		return X, y
 		
-
 	def __len__(self):
-		return len(self.paths)
+		return len(self.dataset)
 
+paths = ['../dataset/test_tif', '../dataset/train_tif', '../dataset/val_tif']
+paths = list(map(Path, paths))
 
+dataset = {}
+dataset['test'], dataset['train'], dataset['validation'] = paths
 
-path = Path("./train/")
+X_test = list(dataset['test'].glob('**/*.TIF'))
 
-path = list(path.glob("**/*.JPEG"))
+dataset_train = Dataset(X_test)
 
-dataset = ConcatDataset(path, "train/", "train_BW/")
-color, gs = dataset.__getitem__(546)
-
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=25, num_workers=0, shuffle=True)
-x,y = next(iter(train_loader))
-
-print(type(x))
+train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=25, num_workers=0, shuffle=True)
+x, y = next(iter(train_loader))
