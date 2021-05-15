76308a363a2bd6d24373fd63b5081f4ae20e6eeb
Livaet <70645720+Livaet@users.noreply.github.com>
2021-05-15 15:54:49 +0200

    Changes to loss - weighting is done in a separate function.

--- src/loss.py
+++ src/loss.py
@@ -2,14 +2,12 @@ import torch
 import pandas as pd
 import numpy as np
 
-
 weight_mix = np.load('weight distribution mix with uniform distribution.npy')
 
 class RarityWeightedLoss():
 
     def __init__(self, pixelProbabilities, lamda, num_bins):
         #distribution = pd.read_csv(pixelProbabilitiesCSV, encoding='UTF-8')
-        self.weighting_factor = PRIOR_PROBS
         self.pixel_dist = torch.tensor(pixelProbabilities)
         self.lamda = lamda
         self.Q = num_bins
@@ -20,24 +18,24 @@ class RarityWeightedLoss():
         #cross entropy of z, z_hat (multiply and sum over q's
         #non_z =  torch.nonzero(prediction)
         #multiply = target[non_z.data]
-        pred = -torch.log(prediction)
-        mul = prediction * target
-        sum = torch.sum(mul, dim=2, keepdim=False)
-        sum_q = sum
+        Z_hat_log = torch.where(prediction > 0, -torch.log(prediction), 0)
+        mul = Z_hat_log * target
+        sum_q = torch.sum(mul, dim=2, keepdim=False)
+        L = -torch.sum(self.weighting(target))*sum_q
         #q = torch.matmul(target, torch.t(-torch.log(prediction)))
         #sum_q = torch.sum(q, dim=2, keepdim=False)
         #get the most likely color
-        pixel_val_for_weight = torch.argmax(target, dim=2)
-        pixel_val_for_weight = torch.flatten(pixel_val_for_weight)
+        # pixel_val_for_weight = torch.argmax(target, dim=2)
+        # pixel_val_for_weight = torch.flatten(pixel_val_for_weight)
         #retrieve it's prior probability
-        probabilities = torch.tensor(self.pixel_dist[pixel_val_for_weight])
-        probabilities = torch.reshape(probabilities, (rows, cols))
+        # probabilities = torch.tensor(self.pixel_dist[pixel_val_for_weight])
+        # probabilities = torch.reshape(probabilities, (rows, cols))
         #mix with normal distribution
-        weights = 1/((1-self.lamda)*probabilities + self.lamda/self.Q)
+        # weights = 1/((1-self.lamda)*probabilities + self.lamda/self.Q)
         #normalize
-        weights = weights/torch.sum(weights)
-        loss = -torch.sum(weights*sum_q)
-        return loss
+        # weights = weights/torch.sum(weights)
+        # loss = -torch.sum(weights*sum_q)
+        return L
 
     def weighting(self, Z):
         most_likely_bin = np.argmax(Z, axis=2) # @todo Check the axis
